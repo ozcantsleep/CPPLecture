9808daeb64ebe33bdd43c3c1d154536501484395
Jun Ahn <brigdesatis@gmail.com>
2023-10-24 16:44:56 +0900

    231024-010

--- L20231024/Project1/main.cpp
+++ L20231024/Project1/main.cpp
@@ -1,10 +1,73 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 
 using namespace std;
 
+/*int main()
+{
+	int A;
+	cin >> A;
+
+	if (A % 2 == 0)
+	{
+		cout << "Â¦¼ö" << endl;
+	}
+	else
+	{
+		cout << "È¦¼ö" << endl;
+	}
+
+	return 0;
+}*/
+
 int main()
 {
-	cout << "Hello World!" << endl;
+	int PlayerX = 1;
+	int PlayerY = 1;
+	char PlayerShape = 'P';
+	bool bIsRunning = true;
+
+	while (bIsRunning)
+	{
+		int Key = _getch();
+
+		system("cls");
+
+		if (Key == 'a' || Key == 'A')
+		{
+			cout << "Left" << endl;
+			PlayerX--;
+		}
+		else if (Key == 'w' || Key == 'W')
+		{
+			cout << "Up" << endl;
+			PlayerY--;
+		}
+		else if (Key == 'd' || Key == 'D')
+		{
+			cout << "Right" << endl;
+			PlayerX++;
+		}
+		else if (Key == 's' || Key == 'S')
+		{
+			cout << "Down" << endl;
+			PlayerY++;
+		}
+		else if (Key == 'q' || Key == 'Q')
+		{
+			cout << "Quit" << endl;
+			bIsRunning = false;
+		}
+
+		cout << PlayerX << ", " << PlayerY << endl;
+
+		//x, y ÁÂÇ¥ ¼³Á¤
+		COORD pos = { PlayerX, PlayerY };
+		//Ä¿¼­ ÀÌµ¿
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+		cout << PlayerShape;
+	}
 
 	return 0;
 }
